4e22c6c67aa20f62ca4f7eb963fbd6c6c52d94cd
Laahkay <lonerkiddow15@gmail.com>
2023-02-17 11:36:44 +0200

    uploding my frontent mentor project

---
+++ mentor/src/App.js
@@ -0,0 +1,21 @@
+import logo from './images/image-qr-code.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="card-qr-code">
+        <img src={logo} className="qr-code" alt="QR Code" />
+         <h2 className="card-title">
+        Improve your font-end skills by building projects
+      </h2>
+      <p className="card-description">
+        Scan the QR code to visit Frontend Mentor and take your coding
+        skills to the next level
+      </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
